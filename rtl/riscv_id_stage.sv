a35cb6894715eed0ae8a50667ccb433630f746be
IGOR LOI <igor.loi@unibo.it>
2018-06-21 17:29:57 +0200

    added instance information when printing Branch decision ix X message

--- rtl/riscv_id_stage.sv
+++ rtl/riscv_id_stage.sv
@@ -1528,7 +1528,7 @@ module riscv_id_stage
   `ifndef VERILATOR
     // make sure that branch decision is valid when jumping
     assert property (
-      @(posedge clk) (branch_in_ex_o) |-> (branch_decision_i !== 1'bx) ) else $display("%t, Branch decision is X", $time);
+      @(posedge clk) (branch_in_ex_o) |-> (branch_decision_i !== 1'bx) ) else $display("%t, Branch decision is X in module %m", $time);
 
     // the instruction delivered to the ID stage should always be valid
     assert property (
