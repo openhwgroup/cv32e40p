9143b6e3f3f24436c6ad997292915ae5780dc562
bluew <bluewww@users.noreply.github.com>
2018-12-18 20:30:39 +0100

    Add .clang-format file and format existing C code

--- tb/core/picorv_firmware/print.c
+++ tb/core/picorv_firmware/print.c
@@ -11,31 +11,30 @@
 
 void print_chr(char ch)
 {
-	*((volatile uint32_t*)OUTPORT) = ch;
+    *((volatile uint32_t *)OUTPORT) = ch;
 }
 
 void print_str(const char *p)
 {
-	while (*p != 0)
-		*((volatile uint32_t*)OUTPORT) = *(p++);
+    while (*p != 0)
+        *((volatile uint32_t *)OUTPORT) = *(p++);
 }
 
 void print_dec(unsigned int val)
 {
-	char buffer[10];
-	char *p = buffer;
-	while (val || p == buffer) {
-		*(p++) = val % 10;
-		val = val / 10;
-	}
-	while (p != buffer) {
-		*((volatile uint32_t*)OUTPORT) = '0' + *(--p);
-	}
+    char buffer[10];
+    char *p = buffer;
+    while (val || p == buffer) {
+        *(p++) = val % 10;
+        val = val / 10;
+    }
+    while (p != buffer) {
+        *((volatile uint32_t *)OUTPORT) = '0' + *(--p);
+    }
 }
 
 void print_hex(unsigned int val, int digits)
 {
-	for (int i = (4*digits)-4; i >= 0; i -= 4)
-		*((volatile uint32_t*)OUTPORT) = "0123456789ABCDEF"[(val >> i) % 16];
+    for (int i = (4 * digits) - 4; i >= 0; i -= 4)
+        *((volatile uint32_t *)OUTPORT) = "0123456789ABCDEF"[(val >> i) % 16];
 }
-
--- tb/core/picorv_firmware/stats.c
+++ tb/core/picorv_firmware/stats.c
@@ -9,34 +9,47 @@
 
 static void stats_print_dec(unsigned int val, int digits, bool zero_pad)
 {
-	char buffer[32];
-	char *p = buffer;
-	while (val || digits > 0) {
-		if (val)
-			*(p++) = '0' + val % 10;
-		else
-			*(p++) = zero_pad ? '0' : ' ';
-		val = val / 10;
-		digits--;
-	}
-	while (p != buffer) {
-		if (p[-1] == ' ' && p[-2] == ' ') p[-1] = '.';
-		print_chr(*(--p));
-	}
+    char buffer[32];
+    char *p = buffer;
+    while (val || digits > 0) {
+	if (val)
+	    *(p++) = '0' + val % 10;
+	else
+	    *(p++) = zero_pad ? '0' : ' ';
+	val = val / 10;
+	digits--;
+    }
+    while (p != buffer) {
+	if (p[-1] == ' ' && p[-2] == ' ')
+	    p[-1] = '.';
+	print_chr(*(--p));
+    }
+}
+
+void init_stats(void)
+{
+    /* enable INSTR anc CYCLE counter */
+    /* TODO: this is still buggy */
+    unsigned int mask = 3;
+    __asm__ volatile("csrw 0x7A1, %0" ::"r"(mask));
 }
 
 void stats(void)
 {
-	unsigned int num_cycles, num_instr;
-	__asm__ volatile ("rdcycle %0; rdinstret %1;" : "=r"(num_cycles), "=r"(num_instr));
-	print_str("Cycle counter ........");
-	stats_print_dec(num_cycles, 8, false);
-	print_str("\nInstruction counter ..");
-	stats_print_dec(num_instr, 8, false);
-	print_str("\nCPI: ");
-	stats_print_dec((num_cycles / num_instr), 0, false);
-	print_str(".");
-	stats_print_dec(((100 * num_cycles) / num_instr) % 100, 2, true);
-	print_str("\n");
+    unsigned int num_cycles, num_instr;
+    /* ideally we could use this */
+    // __asm__ volatile ("rdcycle %0; rdinstret %1;" : "=r"(num_cycles),
+    // "=r"(num_instr));
+    /* riscy specific */
+    __asm__ volatile("csrr %0, 0x780" : "=r"(num_cycles));
+    __asm__ volatile("csrr %0, 0x781" : "=r"(num_instr));
+    print_str("Cycle counter ........");
+    stats_print_dec(num_cycles, 8, false);
+    print_str("\nInstruction counter ..");
+    stats_print_dec(num_instr, 8, false);
+    print_str("\nCPI: ");
+    stats_print_dec((num_cycles / num_instr), 0, false);
+    print_str(".");
+    stats_print_dec(((100 * num_cycles) / num_instr) % 100, 2, true);
+    print_str("\n");
 }
-
