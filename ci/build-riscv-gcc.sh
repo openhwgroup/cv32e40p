11dde5912b2c5dcdf56a79ac60ebe6dec90f1b86
bluew <bluewww@users.noreply.github.com>
2019-09-04 15:49:48 +0000

    Add travis-ci
    We import the CI scripts from ariane as a starting point:
    https://github.com/pulp-platform/ariane/

---
+++ ci/build-riscv-gcc.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# call with first argument = 0 to checkout only
+
+set -e
+ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
+VERSION="691e4e826251c7ec59f883cab18440c87baf45e7"
+
+mkdir -p $RISCV
+
+cd $RISCV
+
+if [ -z ${NUM_JOBS} ]; then
+    NUM_JOBS=1
+fi
+
+
+if ! [ -e $RISCV/bin ]; then
+    if ! [ -e $RISCV/riscv-gnu-toolchain ]; then
+        git clone https://github.com/riscv/riscv-gnu-toolchain.git
+    fi
+
+    cd riscv-gnu-toolchain
+    git checkout $VERSION
+    git submodule update --init --recursive
+
+    if [[ $1 -ne "0" || -z ${1} ]]; then
+      echo "Compiling RISC-V Toolchain"
+      ./configure --prefix=$RISCV --with-arch=rv32gc --with-abi=ilp32
+      make -j${NUM_JOBS} > /dev/null
+      echo "Compilation Finished"
+    fi
+fi
