a8a51d1b6bffd759e7fb0ae0a2eb362ad6bd4dd9
Pasquale Davide Schiavone <pschiavo@larain6.ee.ethz.ch>
2019-09-13 16:35:42 +0200

    porting random stall to the newest core

--- tb/core/mm_ram.sv
+++ tb/core/mm_ram.sv
@@ -47,10 +47,16 @@ module mm_ram
      output logic [31:0]                  exit_value_o);
 
     localparam int                    TIMER_IRQ_ID = 3;
+    localparam int                    PERT_REGS    = 16;
 
     // mux for read and writes
-    enum logic [1:0]{RAM, MM, ERR} select_rdata_d, select_rdata_q;
+    enum logic [1:0]{RAM, MM, PERTURBATION, ERR} select_rdata_d, select_rdata_q;
     logic [31:0]                   data_addr_aligned;
+
+    // signals for handshake
+    logic                          data_rvalid_q;
+    logic                          instr_rvalid_q;
+    logic [INSTR_RDATA_WIDTH-1:0]  core_instr_rdata;
 
     // signals to ram
     logic                          ram_data_req;
@@ -59,6 +65,11 @@ module mm_ram
     logic [31:0]                   ram_data_rdata;
     logic                          ram_data_we;
     logic [3:0]                    ram_data_be;
+    logic [INSTR_RDATA_WIDTH-1:0]  ram_instr_rdata;
+    logic                          ram_instr_req;
+    logic [RAM_ADDR_WIDTH-1:0]     ram_instr_addr;
+    logic                          ram_instr_gnt;
+    logic                          ram_instr_valid;
 
     // signals to print peripheral
     logic [31:0]                   print_wdata;
@@ -76,6 +87,19 @@ module mm_ram
     logic                          timer_val_valid;
     logic [31:0]                   timer_wdata;
 
+    // signals to perturbation
+    logic                          perturbation_data_req;
+    logic [31:0]                   perturbation_wdata;
+    logic [31:0]                   perturbation_addr;
+    logic                          perturbation_we;
+    logic [31:0]                   perturbation_rdata;
+    logic [31:0]                   perturbation_regs [0:PERT_REGS-1];
+    logic                          pert_instr_req;
+    logic [RAM_ADDR_WIDTH-1:0]     pert_instr_addr;
+    logic                          pert_instr_gnt;
+    logic                          pert_instr_valid;
+    logic [INSTR_RDATA_WIDTH-1:0]  pert_instr_rdata;
+
 
     // uhh, align?
     always_comb data_addr_aligned = {data_addr_i[31:2], 2'b0};
@@ -83,24 +107,27 @@ module mm_ram
     // handle the mapping of read and writes to either memory or pseudo
     // peripherals (currently just a redirection of writes to stdout)
     always_comb begin
-        tests_passed_o  = '0;
-        tests_failed_o  = '0;
-        exit_value_o    = 0;
-        exit_valid_o    = '0;
-        ram_data_req    = '0;
-        ram_data_addr   = '0;
-        ram_data_wdata  = '0;
-        ram_data_we     = '0;
-        ram_data_be     = '0;
-        print_wdata     = '0;
-        print_valid     = '0;
-        timer_wdata     = '0;
-        timer_reg_valid = '0;
-        timer_val_valid = '0;
-        sig_end_d       = sig_end_q;
-        sig_begin_d     = sig_begin_q;
-
-        select_rdata_d  = RAM;
+        tests_passed_o          = '0;
+        tests_failed_o          = '0;
+        exit_value_o            =  0;
+        exit_valid_o            = '0;
+        ram_data_req            = '0;
+        ram_data_addr           = '0;
+        ram_data_wdata          = '0;
+        ram_data_we             = '0;
+        ram_data_be             = '0;
+        print_wdata             = '0;
+        print_valid             = '0;
+        timer_wdata             = '0;
+        timer_reg_valid         = '0;
+        timer_val_valid         = '0;
+        sig_end_d               = sig_end_q;
+        sig_begin_d             = sig_begin_q;
+        perturbation_data_req   = '0;
+        perturbation_addr       = '0;
+        perturbation_wdata      = '0;
+        perturbation_we         = '0;
+        select_rdata_d          = RAM;
 
         if (data_req_i) begin
             if (data_we_i) begin // handle writes
@@ -183,6 +210,11 @@ module mm_ram
                     timer_wdata = data_wdata_i;
                     timer_val_valid = '1;
 
+                end else if (data_addr_i[31:16] == 16'h1600) begin
+                    perturbation_data_req  = data_req_i;
+                    perturbation_wdata     = data_wdata_i;
+                    perturbation_addr      = data_addr_i;
+                    perturbation_we        = data_we_i;
                 end else begin
                     // out of bounds write
                 end
@@ -196,7 +228,13 @@ module mm_ram
                     ram_data_wdata = data_wdata_i;
                     ram_data_we = data_we_i;
                     ram_data_be = data_be_i;
+                end else if (data_addr_i[31:16] == 16'h1600) begin
+                    select_rdata_d = PERTURBATION;
 
+                    perturbation_data_req  = data_req_i;
+                    perturbation_wdata     = data_wdata_i;
+                    perturbation_addr      = data_addr_i;
+                    perturbation_we        = data_we_i;
                 end else
                     select_rdata_d = ERR;
 
@@ -216,7 +254,8 @@ module mm_ram
       || data_addr_i == 32'h2000_0004
       || data_addr_i == 32'h2000_0008
       || data_addr_i == 32'h2000_000c
-      || data_addr_i == 32'h2000_0010))
+      || data_addr_i == 32'h2000_0010
+      || data_addr_i[31:16] == 16'h1600))
         else $fatal("out of bounds write to %08x with %08x",
                     data_addr_i, data_wdata_i);
 `endif
@@ -227,6 +266,8 @@ module mm_ram
 
         if(select_rdata_q == RAM) begin
             data_rdata_o = ram_data_rdata;
+        end else if(select_rdata_q == PERTURBATION) begin
+            data_rdata_o = perturbation_rdata;
         end else if (select_rdata_q == ERR) begin
             $display("out of bounds read from %08x", data_addr_i);
             $fatal(2);
@@ -264,7 +305,10 @@ module mm_ram
             timer_irq_mask_q <= '0;
             timer_cnt_q      <= '0;
             irq_q            <= '0;
-
+            for(int i=0; i<PERT_REGS; i++) begin
+                perturbation_regs[i] <= '0;
+            end
+            perturbation_rdata  <= '0;
         end else begin
             // set timer irq mask
             if(timer_reg_valid) begin
@@ -274,6 +318,11 @@ module mm_ram
             end else if(timer_val_valid) begin
                 timer_cnt_q <= timer_wdata;
 
+            end else if(perturbation_data_req) begin
+                if(perturbation_we)
+                    perturbation_regs[perturbation_addr[5:2]] <= perturbation_wdata;
+                else
+                    perturbation_rdata <= perturbation_regs[perturbation_addr[5:2]];
             end else begin
                 if(timer_cnt_q > 0)
                     timer_cnt_q <= timer_cnt_q - 1;
@@ -301,14 +350,14 @@ module mm_ram
           .INSTR_RDATA_WIDTH(INSTR_RDATA_WIDTH))
     dp_ram_i
         (
-         .clk_i     ( clk_i         ),
+         .clk_i     ( clk_i           ),
 
-         .en_a_i    ( instr_req_i   ),
-         .addr_a_i  ( instr_addr_i  ),
-         .wdata_a_i ( '0            ),	// Not writing so ignored
-         .rdata_a_o ( instr_rdata_o ),
-         .we_a_i    ( '0            ),
-         .be_a_i    ( 4'b1111       ),	// Always want 32-bits
+         .en_a_i    ( ram_instr_req   ),
+         .addr_a_i  ( ram_instr_addr  ),
+         .wdata_a_i ( '0              ),	// Not writing so ignored
+         .rdata_a_o ( ram_instr_rdata ),
+         .we_a_i    ( '0              ),
+         .be_a_i    ( 4'b1111         ),	// Always want 32-bits
 
          .en_b_i    ( ram_data_req    ),
          .addr_b_i  ( ram_data_addr   ),
@@ -317,9 +366,6 @@ module mm_ram
          .we_b_i    ( ram_data_we     ),
          .be_b_i    ( ram_data_be     ));
 
-    // do the handshacking stuff by assuming we always react in one cycle
-    assign data_gnt_o  = data_req_i;
-    assign instr_gnt_o = instr_req_i;
 
     // signature range
     always_ff @(posedge clk_i, negedge rst_ni) begin
@@ -337,15 +383,80 @@ module mm_ram
     always_ff @(posedge clk_i, negedge rst_ni) begin
         if (~rst_ni) begin
             select_rdata_q <= RAM;
-            data_rvalid_o  <= '0;
-            instr_rvalid_o <= '0;
+            data_rvalid_q  <= '0;
+            instr_rvalid_q <= '0;
 
         end else begin
             select_rdata_q <= select_rdata_d;
-            data_rvalid_o  <= data_req_i;
-            instr_rvalid_o <= instr_req_i;
+            data_rvalid_q  <= data_req_i;
+            instr_rvalid_q <= ram_instr_req;
 
         end
     end
+
+    // do the handshacking stuff by assuming we always react in one cycle
+    assign data_gnt_o     = data_req_i;
+    assign data_rvalid_o  = data_rvalid_q;
+
+    assign instr_gnt_o    = ram_instr_gnt;
+    assign instr_rvalid_o = ram_instr_valid;
+    assign instr_rdata_o  = core_instr_rdata;
+
+
+`ifndef VERILATOR
+  always_comb
+  begin
+    ram_instr_req    = instr_req_i;
+    ram_instr_addr   = instr_addr_i;
+    ram_instr_gnt    = instr_req_i;
+    ram_instr_valid  = instr_rvalid_q;
+    core_instr_rdata = ram_instr_rdata;
+
+    if(perturbation_regs[0]) begin
+        ram_instr_req    = pert_instr_req;
+        ram_instr_addr   = pert_instr_addr;
+        ram_instr_gnt    = pert_instr_gnt;
+        ram_instr_valid  = pert_instr_valid;
+        core_instr_rdata = pert_instr_rdata;
+    end
+  end
+
+  riscv_random_stall
+  #(.DATA_WIDTH(INSTR_RDATA_WIDTH))
+  instr_random_stalls
+  (
+    .clk_i              ( clk_i             ),
+    .rst_ni             ( rst_ni            ),
+
+    .grant_mem_i        ( pert_instr_req    ),
+    .rvalid_mem_i       ( instr_rvalid_q    ),
+    .rdata_mem_i        ( ram_instr_rdata   ),
+
+    .grant_core_o       ( pert_instr_gnt    ),
+    .rvalid_core_o      ( pert_instr_valid  ),
+    .rdata_core_o       ( pert_instr_rdata  ),
+
+    .req_core_i         ( instr_req_i       ),
+    .req_mem_o          ( pert_instr_req    ),
+
+    .addr_core_i        ( instr_addr_i      ),
+    .addr_mem_o         ( pert_instr_addr   ),
+
+    .wdata_core_i       (                   ),
+    .wdata_mem_o        (                   ),
+
+    .we_core_i          (                   ),
+    .we_mem_o           (                   ),
+
+    .be_core_i          (                   ),
+    .be_mem_o           (                   ),
+
+    .stall_mode_i       ( 32'h1             ),
+    .max_stall_i        ( 32'h3             ),
+    .gnt_stall_i        ( 32'h2             ),
+    .valid_stall_i      ( 32'h1             )
+    );
+
+`endif
 
 endmodule // ram
