acb2f099c17fb14d3692ac08b5b532c1b9d7dba4
mp-17 <mperotti@iis.ee.ethz.ch>
2020-04-03 18:19:13 +0200

    :bug: fix memory stall activation routine

--- tb/core/custom/hello_world.c
+++ tb/core/custom/hello_world.c
@@ -12,31 +12,33 @@ void activate_random_stall(void)
     rnd_stall_reg[i] = rnd_stall_reg[i-1] + 1; // It is a pointer to int ("+ 1" means "the next int")
   }
 
-  // INSTRUCTION MEMORY
-  // Set stall mode (standard=1, random=2) (rnd_stall_reg[2])
-  *rnd_stall_reg[2] = 0x02;
-  // Set max n. stalls on both GNT and VALID for RANDOM mode (rnd_stall_reg[4])
-  *rnd_stall_reg[4] = 0x0a;
-  // Set n. stalls on  GNT (rnd_stall_reg[6])
-  *rnd_stall_reg[6] = 0x00;
-  // Set n. stalls on VALID (rnd_stall_reg[8])
-  *rnd_stall_reg[8] = 0x00;
+  /* The interposition of the stall generator between CPU and MEM should happen BEFORE the stall generetor is active */
+  // Interpose the stall generator between CPU and D-MEM (rnd_stall_reg[1])
+  *rnd_stall_reg[1] = 0x01;
+  // Interpose the stall generator between CPU and I-MEM (rnd_stall_reg[0])
+  *rnd_stall_reg[0] = 0x00;
 
   // DATA MEMORY
-  // Set stall mode (standard=1, random=2) (rnd_stall_reg[3])
-  *rnd_stall_reg[3] = 0x02;
   // Set max n. stalls on both GNT and VALID for RANDOM mode (rnd_stall_reg[5])
-  *rnd_stall_reg[5] = 0x0a;
+  *rnd_stall_reg[5] = 0x00;
   // Set n. stalls on  GNT (rnd_stall_reg[7])
   *rnd_stall_reg[7] = 0x00;
   // Set n. stalls on VALID (rnd_stall_reg[9])
   *rnd_stall_reg[9] = 0x00;
 
+  // INSTRUCTION MEMORY
+  // Set max n. stalls on both GNT and VALID for RANDOM mode (rnd_stall_reg[4])
+  *rnd_stall_reg[4] = 0x00;
+  // Set n. stalls on  GNT (rnd_stall_reg[6])
+  *rnd_stall_reg[6] = 0x00;
+  // Set n. stalls on VALID (rnd_stall_reg[8])
+  *rnd_stall_reg[8] = 0x05;
+
   /* Activating stalls on D and I Mem has to be done as last operation. Do not change the order. */
-  // Enable/disable stalls on D-MEM (rnd_stall_reg[1])
-  *rnd_stall_reg[1] = 0x01;
-  // Enable/disable stalls on I-MEM (rnd_stall_reg[0])
-  *rnd_stall_reg[0] = 0x01;
+  // Set stall mode on D-MEM (off=0, standard=1, random=2) (rnd_stall_reg[3])
+  *rnd_stall_reg[3] = 0x01;
+  // Set stall mode on I-MEM (off=0, standard=1, random=2) (rnd_stall_reg[2])
+  *rnd_stall_reg[2] = 0x00;
 }
 
 int main(int argc, char *argv[])
