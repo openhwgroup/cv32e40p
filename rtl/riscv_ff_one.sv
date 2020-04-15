99d4b66e97dbe737aa3f6b63dbf814de7d4fe9be
Pasquale Davide Schiavone <pschiavo@larain6.ee.ethz.ch>
2020-04-15 11:51:45 +0200

    :art: moved to different files find fist one and population count modules

---
+++ rtl/riscv_ff_one.sv
@@ -0,0 +1,98 @@
+// Copyright 2018 ETH Zurich and University of Bologna.
+// Copyright and related rights are licensed under the Solderpad Hardware
+// License, Version 0.51 (the "License"); you may not use this file except in
+// compliance with the License.  You may obtain a copy of the License at
+// http://solderpad.org/licenses/SHL-0.51. Unless required by applicable law
+// or agreed to in writing, software, hardware and materials distributed under
+// this License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+
+////////////////////////////////////////////////////////////////////////////////
+// Engineer:       Andreas Traber - atraber@student.ethz.ch                   //
+//                                                                            //
+// Additional contributions by:                                               //
+//                 Davide Schiavone - pschiavo@iis.ee.ethz.ch                 //
+//                                                                            //
+// Design Name:    riscv_ff_one                                               //
+// Project Name:   RI5CY                                                      //
+// Language:       SystemVerilog                                              //
+//                                                                            //
+// Description:    Find First One                                             //
+//                                                                            //
+////////////////////////////////////////////////////////////////////////////////
+
+module riscv_ff_one
+#(
+  parameter LEN = 32
+)
+(
+  input  logic [LEN-1:0]         in_i,
+
+  output logic [$clog2(LEN)-1:0] first_one_o,
+  output logic                   no_ones_o
+);
+
+  localparam NUM_LEVELS = $clog2(LEN);
+
+  logic [LEN-1:0] [NUM_LEVELS-1:0]           index_lut;
+  logic [2**NUM_LEVELS-1:0]                  sel_nodes;
+  logic [2**NUM_LEVELS-1:0] [NUM_LEVELS-1:0] index_nodes;
+
+
+  //////////////////////////////////////////////////////////////////////////////
+  // generate tree structure
+  //////////////////////////////////////////////////////////////////////////////
+
+  generate
+    genvar j;
+    for (j = 0; j < LEN; j++) begin
+      assign index_lut[j] = $unsigned(j);
+    end
+  endgenerate
+
+  generate
+    genvar k;
+    genvar l;
+    genvar level;
+    for (level = 0; level < NUM_LEVELS; level++) begin
+    //------------------------------------------------------------
+    if (level < NUM_LEVELS-1) begin
+      for (l = 0; l < 2**level; l++) begin
+        assign sel_nodes[2**level-1+l]   = sel_nodes[2**(level+1)-1+l*2] | sel_nodes[2**(level+1)-1+l*2+1];
+        assign index_nodes[2**level-1+l] = (sel_nodes[2**(level+1)-1+l*2] == 1'b1) ?
+                                           index_nodes[2**(level+1)-1+l*2] : index_nodes[2**(level+1)-1+l*2+1];
+      end
+    end
+    //------------------------------------------------------------
+    if (level == NUM_LEVELS-1) begin
+      for (k = 0; k < 2**level; k++) begin
+        // if two successive indices are still in the vector...
+        if (k * 2 < LEN-1) begin
+          assign sel_nodes[2**level-1+k]   = in_i[k*2] | in_i[k*2+1];
+          assign index_nodes[2**level-1+k] = (in_i[k*2] == 1'b1) ? index_lut[k*2] : index_lut[k*2+1];
+        end
+        // if only the first index is still in the vector...
+        if (k * 2 == LEN-1) begin
+          assign sel_nodes[2**level-1+k]   = in_i[k*2];
+          assign index_nodes[2**level-1+k] = index_lut[k*2];
+        end
+        // if index is out of range
+        if (k * 2 > LEN-1) begin
+          assign sel_nodes[2**level-1+k]   = 1'b0;
+          assign index_nodes[2**level-1+k] = '0;
+        end
+      end
+    end
+    //------------------------------------------------------------
+    end
+  endgenerate
+
+  //////////////////////////////////////////////////////////////////////////////
+  // connect output
+  //////////////////////////////////////////////////////////////////////////////
+
+  assign first_one_o = index_nodes[0];
+  assign no_ones_o   = ~sel_nodes[0];
+
+endmodule
