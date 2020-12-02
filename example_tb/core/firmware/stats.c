3c1bd46536dda0eb660f3574f8b4fc075f19dedf
Robert Balas <balasr@iis.ee.ethz.ch>
2020-12-02 14:37:18 +0100

    Add missing license headers
    Signed-off-by: Robert Balas <balasr@iis.ee.ethz.ch>

--- example_tb/core/firmware/stats.c
+++ example_tb/core/firmware/stats.c
@@ -1,9 +1,19 @@
-// This is free and unencumbered software released into the public domain.
-//
-// Anyone is free to copy, modify, publish, use, compile, sell, or
-// distribute this software, either in source code form or as a compiled
-// binary, for any purpose, commercial or non-commercial, and by any
-// means.
+/*
+ * Copyright 2020 ETH Zurich
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 
 #include "firmware.h"
 
