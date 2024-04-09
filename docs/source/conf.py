5aa98cb3f225c876a779134ab01585f7ad03e361
Pascal Gouedo <pascal.gouedo@dolphin.fr>
2024-04-09 17:13:42 +0200

    Forbid floating figures.
    Signed-off-by: Pascal Gouedo <pascal.gouedo@dolphin.fr>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -164,6 +164,7 @@ latex_elements = {
     # Latex figure (float) alignment
     #
     # 'figure_align': 'htbp',
+    'figure_align': 'H',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
