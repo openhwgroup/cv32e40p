64e6f27714f9d8eec1c1fcc08e318ba9f9239127
Pascal Gouedo <pascal.gouedo@dolphin.fr>
2024-04-18 13:01:25 +0200

    Updated User Manual release to v1.8.0
    Signed-off-by: Pascal Gouedo <pascal.gouedo@dolphin.fr>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,7 +44,7 @@ author = u'PULP Platform and OpenHW Group'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'v1.7.1'
+release = u'v1.8.0'
 
 
 # -- General configuration ---------------------------------------------------
