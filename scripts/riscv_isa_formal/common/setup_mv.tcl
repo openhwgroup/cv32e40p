0a9df7266b205be743a67354ba671317dfc1ce93
Pascal Gouedo <pascal.gouedo@dolphin.fr>
2024-06-03 17:32:19 +0200

    Reverted PR #993.

--- scripts/riscv_isa_formal/common/setup_mv.tcl
+++
@@ -1,8 +0,0 @@
-if {![info exists ::env(DESIGN_RTL_DIR)]} {
-	set ::env(DESIGN_RTL_DIR) [pwd]/rtl
-}
-set_read_hdl_option -verilog_version sv2012 -pragma_ignore {translate_}
-vlog -sv -f cv32e40p_fpu_manifest.flist
-elaborate 
-compile
-set_mode mv
