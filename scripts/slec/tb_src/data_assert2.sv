4951be455cf39166fabb1f78f132db9f478c2035
mret55 <ishan.dalal@cirrus.com>
2024-06-17 08:15:32 -0500

    Replaced headers in assume and bind files with the ones in formal dir. Uncommented the row4 trigger_match_i assertion from controller_assert file

--- scripts/slec/tb_src/data_assert2.sv
+++ scripts/slec/tb_src/data_assert2.sv
@@ -1,5 +1,28 @@
-// Copyright 2024 Cirrus Logic
+// Copyright 2024 Dolphin Design
 // SPDX-License-Identifier: Apache-2.0 WITH SHL-2.1
+//
+// Licensed under the Solderpad Hardware License v 2.1 (the "License");
+// you may not use this file except in compliance with the License, or,
+// at your option, the Apache License version 2.0.
+// You may obtain a copy of the License at
+//
+// https://solderpad.org/licenses/SHL-2.1/
+//
+// Unless required by applicable law or agreed to in writing, any work
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+////////////////////////////////////////////////////////////////////////////////////
+//                                                                                //
+// Contributors: Yoann Pruvost, Dolphin Design <yoann.pruvost@dolphin.fr>         //
+//               Pascal Gouedo, Dolphin Design <pascal.gouedo@dolphin.fr>         //
+//                                                                                //
+// Description:  OBI protocol emulation for CV32E40P data interface               //
+//                                                                                //
+////////////////////////////////////////////////////////////////////////////////////
+
 module data_assert (
     input logic clk_i,
     input logic rst_ni,
