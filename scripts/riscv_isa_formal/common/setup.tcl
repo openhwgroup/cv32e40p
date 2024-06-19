e77b13bd19f49ef9d2e651fde06ce34c884d34f4
Pascal Gouedo <pascal.gouedo@dolphin.fr>
2024-06-19 12:24:16 +0200

    License header addition except in json files which don't accept comments.
    Signed-off-by: Pascal Gouedo <pascal.gouedo@dolphin.fr>

--- scripts/riscv_isa_formal/common/setup.tcl
+++ scripts/riscv_isa_formal/common/setup.tcl
@@ -1,3 +1,19 @@
+# Copyright 2024 Siemens EDA
+# SPDX-License-Identifier: Apache-2.0 WITH SHL-2.1
+# 
+# Licensed under the Solderpad Hardware License v 2.1 (the "License");
+# you may not use this file except in compliance with the License, or,
+# at your option, the Apache License version 2.0.
+# You may obtain a copy of the License at
+# 
+# https://solderpad.org/licenses/SHL-2.1/
+# 
+# Unless required by applicable law or agreed to in writing, any work
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 proc duration {int_time} {
     set timeList [list]
     if {$int_time == 0} {
